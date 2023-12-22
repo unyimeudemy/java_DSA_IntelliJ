5b4e05d60614c0207460c202ab2676cedfa44e87
unyimeudoh <unyimeudemy20@gmail.com>
2023-12-22 23:27:05 +0100

    heap data structure

--- src/main/java/Heap/MaximumHeap.java
+++ src/main/java/Heap/MaximumHeap.java
@@ -1,4 +1,40 @@
 package Heap;
 
+import java.util.ArrayList;
+
 public class MaximumHeap {
+    static ArrayList<Integer> arr = new ArrayList<Integer>();
+
+    public static void main(String[] args){
+        MaximumHeap myHeap = new MaximumHeap();
+        myHeap.insert(2);
+        myHeap.insert(5);
+        myHeap.insert(1);
+        myHeap.insert(9);
+        myHeap.insert(4);
+        myHeap.insert(0);
+        myHeap.insert(3);
+        myHeap.insert(7);
+
+        System.out.println(arr);
+    }
+
+    public void insert(int numberToInsert){
+        arr.add(numberToInsert);
+        int indexOfNewElement = arr.size() - 1;
+        while(indexOfNewElement > 0){
+            int indexOfParent = (indexOfNewElement - 1)/2;
+            if(arr.get(indexOfNewElement) <= arr.get(indexOfParent)){
+                break;
+            }
+            swap(indexOfNewElement, indexOfParent);
+            indexOfNewElement = indexOfParent;
+        }
+    }
+
+    private void swap(int first, int second){
+        int valueAtFirst = arr.get(first);
+        arr.set(first, arr.get(second));
+        arr.set(second, valueAtFirst);
+    }
 }
--- src/main/java/Heap/MinimumHeap.java
+++ src/main/java/Heap/MinimumHeap.java
@@ -20,21 +20,8 @@ public class MinimumHeap {
         myHeap.insert(7);
 
         System.out.println(arr);
-    }
-
-    public void insert1(int numberToInsert) {
-        arr.add(numberToInsert);
-        int indexOfNewElement = arr.size() - 1;
-        while (indexOfNewElement > 0) {
-            int indexOfParent = (indexOfNewElement - 1) / 2;
-            if (arr.get(indexOfNewElement) >= arr.get(indexOfParent)) {
-                break;
-            }
-            int temp = arr.get(indexOfNewElement);
-            arr.set(indexOfNewElement, arr.get(indexOfParent));
-            arr.set(indexOfParent, temp);
-            indexOfNewElement = indexOfParent;
-        }
+        int min = myHeap.getMin();
+        System.out.println(arr);
     }
 
 
@@ -46,40 +33,50 @@ public class MinimumHeap {
             if(arr.get(indexOfNewElement) >= arr.get(indexOfParent)){
                 break;
             }
-            int temp = arr.get(indexOfNewElement);
-            arr.set(indexOfNewElement, arr.get(indexOfParent));
-            arr.set(indexOfParent, temp);
+            swap(indexOfNewElement, indexOfParent);
             indexOfNewElement = indexOfParent;
         }
     }
+
+
+
+
     public int getMin(){
         if(arr.size() == 0){
-            return -1;
+            throw new IllegalStateException("Heap is empty");
         }
-        int min = arr.get(0);
-        if(arr.size() == 1) {
-            return min;
-        }
-        int lastElement = arr.get(arr.size() - 1);
-        arr.set(0, lastElement);
-        int leftChildIndex = 1;
-        int rightChildIndex = 2;
-        while(lastElement > arr.get(leftChildIndex) || lastElement > arr.get(rightChildIndex)){
-            int leftChild = arr.get(leftChildIndex);
-            int rightChild = arr.get(rightChildIndex);
-            if(arr.get(leftChildIndex) < arr.get(rightChildIndex)){
-                arr.set(arr.indexOf(lastElement), leftChild);
-                arr.set(leftChildIndex, lastElement);
-                leftChildIndex = arr.indexOf(leftChild);
-            }
+      int min = arr.get(0);
+        int movedElement = arr.remove(arr.size() - 1);
+        arr.set(0, movedElement);
+      int movedElementIndex = 0;
+      while(true){
+          int leftChildIndex = 2 * movedElementIndex + 1;
+          int rightChildIndex = 2 * movedElementIndex + 2;
 
-            if(arr.get(rightChildIndex) < arr.get(leftChildIndex) ){
-                arr.set(arr.indexOf(lastElement), rightChild);
-                arr.set(arr.indexOf(rightChild), lastElement);
-                rightChildIndex = arr.indexOf(rightChild);
-            }
-        }
-        return min;
+          if(leftChildIndex >= arr.size()){
+              break;
+          }
 
+          int smallerChildIndex = leftChildIndex;
+          if(rightChildIndex < arr.size() &&  arr.get(rightChildIndex) < arr.get(leftChildIndex)){
+              smallerChildIndex = rightChildIndex;
+          }
+
+          if(arr.get(movedElementIndex) <= arr.get(smallerChildIndex)){
+              break;
+          }
+
+          swap(movedElementIndex, smallerChildIndex);
+          movedElementIndex = smallerChildIndex;
+      }
+
+      return min;
+    }
+//
+
+    private void swap (int first, int second){
+        int valueAtFirst = arr.get(first);
+        arr.set(first, arr.get(second));
+        arr.set(second, valueAtFirst);
     }
 }
