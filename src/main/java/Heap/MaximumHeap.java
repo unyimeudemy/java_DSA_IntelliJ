1322675fdeba8dd239dc46cec1fc4a68e7eda38e
unyimeudoh <unyimeudemy20@gmail.com>
2023-12-24 20:19:41 +0100

    comments added

--- src/main/java/Heap/MaximumHeap.java
+++ src/main/java/Heap/MaximumHeap.java
@@ -26,7 +26,7 @@ public class MaximumHeap {
     public void insert(int numberToInsert){
         arr.add(numberToInsert);
         int indexOfNewElement = arr.size() - 1;
-        upHeap(indexOfNewElement);
+        downHeap(indexOfNewElement);
     }
 
     public int getMax(){
@@ -38,7 +38,7 @@ public class MaximumHeap {
         int lastElement = arr.remove(arr.size() - 1);
         arr.set(0, lastElement);
         int indexOfMovedElement = 0;
-        downHeap(indexOfMovedElement);
+        upHeap(indexOfMovedElement);
         return max;
     }
 
@@ -48,34 +48,59 @@ public class MaximumHeap {
         }
     }
 
-    private void downHeap(int indexOfMovedElement){
+    private void upHeap(int indexOfMovedElement){
         while(true){
+            //get left and right children
             int leftChildIndex = 2 * indexOfMovedElement + 1;
             int rightChildIndex = 2 * indexOfMovedElement + 2;
 
+
+            //ensure heap has at least one child.
             if(leftChildIndex >= arr.size()){
                 break;
             }
+
+            //determine the child node to swap depending on which one is bigger
             int biggerChildIndex = leftChildIndex;
             if(rightChildIndex < arr.size() && arr.get(rightChildIndex) > arr.get(leftChildIndex)){
                 biggerChildIndex = rightChildIndex;
             }
 
+            //Ensures that the swapping operation meets the condition of a
+            //maximum heap. That is, the parent node should be higher
+            //to the child node.
             if(arr.get(indexOfMovedElement) >= arr.get(biggerChildIndex)){
                 break;
             }
+
+            //swaps the position between the parent ( or index of element moving down the heap )
+            //node and the child that is bigger.
             swap(indexOfMovedElement, biggerChildIndex);
+
+            //set the index the parent ( or index of element moving down the heap )
+            //to the index of the child node that has just been swapped since the
+            // moving node as just come down.
             indexOfMovedElement = biggerChildIndex;
         }
     }
 
-    private void upHeap(int indexOfNewElement){
+    private void downHeap(int indexOfNewElement){
         while(indexOfNewElement > 0){
+            //get parent index
             int indexOfParent = (indexOfNewElement - 1)/2;
+
+            //ensure that the operation satisfies the condition of a maximum
+            //heap. The parent node should be greater than the child.
             if(arr.get(indexOfNewElement) <= arr.get(indexOfParent)){
                 break;
             }
+
+            //swaps the element in child node with the parent if it child's
+            // current position violates the rule of max heap.
             swap(indexOfNewElement, indexOfParent);
+
+            //set the index of the just moved node to the position of
+            // parent since it has just moved up.
             indexOfNewElement = indexOfParent;
         }
     }
