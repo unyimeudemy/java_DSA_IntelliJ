b14848c128d8c48261cfb75ce6b8b88ace423b99
unyimeudoh <unyimeudemy20@gmail.com>
2023-12-30 08:20:28 +0100

    collection framework

---
+++ src/main/java/OOP/Generics.java
@@ -0,0 +1,27 @@
+package OOP;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Generics {
+    public static void main(String[] args) {
+
+    }
+}
+
+class CustomArrayList{
+    private int[] data;
+    private static int DEFAULT_SIZE = 10;
+
+    void testMethod (){
+        ArrayList arr = new ArrayList();
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        Number num;
+    }
+
+    void getList(List<? extends Number> list){
+
+    }
+
+}
