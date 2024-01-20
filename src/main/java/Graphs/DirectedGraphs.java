46a5c77cd87638cd2cafd38a5030d9641ef5cc97
unyimeudoh <unyimeudemy20@gmail.com>
2024-01-20 16:27:25 +0100

    digraphs finished

--- src/main/java/Graphs/DirectedGraphs.java
+++ src/main/java/Graphs/DirectedGraphs.java
@@ -2,25 +2,40 @@ package Graphs;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 public class DirectedGraphs {
     public static void main(String[] args) {
-        DiGraph myGraph = new DiGraph(8);
-        myGraph.addEdge(0, 1);
-        myGraph.addEdge(0, 2);
-        myGraph.addEdge(0, 3);
-        myGraph.addEdge(1, 3);
-        myGraph.addEdge(2, 4);
-        myGraph.addEdge(4, 3);
-        myGraph.addEdge(5, 6);
-        myGraph.addEdge(6, 7);
+//        DiGraph myGraph = new DiGraph(7);
+//        myGraph.addEdge(0, 5);
+//        myGraph.addEdge(0, 2);
+//        myGraph.addEdge(0, 1);
+//        myGraph.addEdge(3, 6);
+//        myGraph.addEdge(3, 5);
+//        myGraph.addEdge(3, 4);
+//        myGraph.addEdge(5, 2);
+//        myGraph.addEdge(6, 4);
+//        myGraph.addEdge(6, 0);
+//        myGraph.addEdge(3, 2);
+//        myGraph.addEdge(1, 4);
 
+        DiGraph g = new DiGraph(6);
+        g.addEdge(5, 2);
+        g.addEdge(5, 0);
+        g.addEdge(4, 0);
+        g.addEdge(4, 1);
+        g.addEdge(2, 3);
+        g.addEdge(3, 1);
 
-        for (int i = 0; i < myGraph.size(); i++) {
-            for (int currentVertex: myGraph.adj(i)) {
-                System.out.println(i + " -> " + currentVertex);
-            }
-        }
+//        for (int i = 0; i < myGraph.size(); i++) {
+//            for (int currentVertex: myGraph.adj(i)) {
+//                System.out.println(i + " -> " + currentVertex);
+//            }
+//        }
+
+        TopoSort sortedGraph = new TopoSort(g);
+        System.out.println(sortedGraph.reversePost());
+        sortedGraph.display();
     }
 
 }
@@ -76,7 +91,6 @@ class DepthFirstPathDigraph{
             }
         }
     }
-
 
 }
 
@@ -152,6 +166,41 @@ class BreadthFirstPathDigraph{
                     edgedTo[vertex] = currentVertex;
                 }
             }
+        }
+    }
+}
+
+class TopoSort{
+    private boolean[] marked;
+    private Stack<Integer> reversePost;
+
+    public TopoSort(DiGraph graph){
+        marked = new boolean[graph.size()];
+        reversePost = new Stack<>();
+        for (int vertex = 0; vertex < graph.size(); vertex++) {
+            if(!marked[vertex]){
+                dfs(graph, vertex);
+            }
+        }
+    }
+
+    private void dfs(DiGraph graph, int targetVertex){
+        marked[targetVertex] = true;
+        for(int currentVertex: graph.adj(targetVertex)){
+            if(!marked[currentVertex]){
+                dfs(graph, currentVertex);
+            }
+        }
+        reversePost.push(targetVertex);
+    }
+
+    public Iterable<Integer> reversePost(){
+        return reversePost;
+    }
+
+    public void display(){
+        while(!reversePost.isEmpty()){
+            System.out.print(reversePost.pop() + " ");
         }
     }
 }
